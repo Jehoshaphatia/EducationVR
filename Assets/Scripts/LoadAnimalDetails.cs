1e30a8ac9056f32821a96527ae7edf26d90f4c90
Jehoshaphat Abu <jehoshaphatia@outlook.com>
2018-08-12 18:54:11 +0100

    Feat: add rabbit detail

--- Assets/Scripts/LoadAnimalDetails.cs
+++ Assets/Scripts/LoadAnimalDetails.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LoadAnimalDetails : MonoBehaviour {
+    public Canvas catCanvas;
+    public Canvas chimpCanvas;
+    public Canvas dogCanvas;
+    public Canvas gallinaCanvas;
+    public Canvas GiraffeCanvas;
+    public Canvas horseCanvas;
+    public Canvas pigCanvas;
+    public Canvas rabbitCanvas;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +21,10 @@ public class LoadAnimalDetails : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    //Use this for 
+    public void loadCatDetails()
+    {
+
+    }
 }
